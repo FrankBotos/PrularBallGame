484aa062bc17c26802f5506b60739bf0762d13df
FrankBotos <frankbotos@hotmail.com>
2017-10-23 14:38:15 -0400

    Initial Files Added
    This is the initial commit! Game is in a very basic experiment state.

---
+++ Tims_Balls/Tims_Balls/Assets/Scripts/BallBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallBehaviour : MonoBehaviour {
+
+	private int dir = 0;
+	public float movementSpeed = 500f;
+
+	// Use this for initialization
+	void Start () {
+		dir = Random.Range (0,2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (dir == 1) {
+			//Debug.Log ("We're going up!");
+			transform.Translate(Vector3.up * movementSpeed  * Time.deltaTime);
+		} else {
+			//Debug.Log ("We're going down!");
+			transform.Translate(Vector3.down * movementSpeed * Time.deltaTime);
+		}
+	}
+}
---
+++ Tims_Balls/Tims_Balls/Assets/Scripts/TouchManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//each time the game updates we check for a "mouse left click" or "tap" and convert it from world space to screen space
+		if (Input.GetMouseButton (0)) {
+			Vector3 getMousePositionFar = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, Camera.main.farClipPlane);
+			Vector3 getMousePositionNear = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane);
+
+			Vector3 mousePosFar = Camera.main.ScreenToWorldPoint (getMousePositionFar);
+			Vector3 mousePosNear = Camera.main.ScreenToWorldPoint (getMousePositionNear);
+	
+			//This is a debug raycast, it lets you see the raycast in the scene view if needed
+			Debug.DrawRay(mousePosNear, mousePosFar - mousePosNear, Color.red);
+
+
+			//we send out a ray cast during each "click" or "tap" and destroy the object that has been clicked
+			RaycastHit hit;
+			if (Physics.Raycast (mousePosNear, mousePosFar - mousePosNear, out hit)) {
+				Destroy (hit.transform.gameObject);
+			}
+
+		}
+	}
+}
