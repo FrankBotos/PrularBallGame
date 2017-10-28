7f21169acdeb192b6af80335fe8509cbf857b06b
FrankBotos <frankbotos@hotmail.com>
2017-10-28 12:27:10 -0400

    Basic "game over" functionality
    The balls will now destroy themselves if they move past camera bounds,
    and reset score to zero.

--- Tims_Balls/Tims_Balls/Assets/Scripts/BallBehaviour.cs
+++ Tims_Balls/Tims_Balls/Assets/Scripts/BallBehaviour.cs
@@ -5,14 +5,13 @@ public class BallBehaviour : MonoBehaviour {
 	
 	public float moveSpeed = 5.0f;
 	public bool ballGoesUp = false;
+	public bool isVisibleByCamera;
 
-	// Use this for initialization
-	void Start () {
+	void Start(){
 	}
 
 	// Update is called once per frame
 	void Update () {
-
 		//move
 		if (ballGoesUp == true) {
 			transform.Translate (moveSpeed * Vector3.up * Time.deltaTime);
@@ -20,14 +19,37 @@ public class BallBehaviour : MonoBehaviour {
 			transform.Translate (moveSpeed * Vector3.down * Time.deltaTime);
 		}
 
-	}
+		//check if visible by camera
+		//we check if ball is within y fulstrum of camera
+		Vector3 screenPoint = Camera.main.WorldToViewportPoint(transform.position);
+		isVisibleByCamera = screenPoint.y > 0 && screenPoint.y < 1;
+		if (!isVisibleByCamera){
+			Destroy (gameObject);
+		}
 
+
+	}
+		
 	void OnDestroy(){
 
-		//when ball is destroyed we find the "Text" object, then get a reference to the score script attached to that object, then update the score
-		GameObject tempRef = GameObject.Find ("Text");
-		ScoreController scoreRef = tempRef.GetComponent<ScoreController> ();
-		scoreRef.score++;
+
+		//we increase the score if the ball is visible by the camera only, since that means the player clicked
+		if (isVisibleByCamera) {
+			//when ball is destroyed we find the "Text" object, then get a reference to the score script attached to that object, then update the score
+			GameObject tempRef = GameObject.Find ("Text");
+			ScoreController scoreRef = tempRef.GetComponent<ScoreController> ();
+			scoreRef.score++;
+		} else {//otherwise, game over and we reset the game
+			Debug.Log ("Game Over!");
+
+			//temporary game over solution
+			GameObject tempRef = GameObject.Find ("Text");
+			ScoreController scoreRef = tempRef.GetComponent<ScoreController> ();
+			scoreRef.score = 0;
+
+		}
+
+
 
 	}
 
